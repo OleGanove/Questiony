43312e43041b17129b6bdffe6bd63e7898728342
Ngoc Truong <truong@tutanota.de>
2015-10-09 16:17:18 +0200

    Change error message when User answer question twice

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,5 +3,5 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   # Users can only answer a question once
-  validates :user_id, uniqueness: { scope: [:question_id] }
+  validates :user_id, uniqueness: { scope: [:question_id], message: "You have already answered the question" }
 end
